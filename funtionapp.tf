52ae9b7eedbfc4a571d24b330391127cc50a4948
Junior Castillo <castillojunior099@gmail.com>
2025-08-05 23:03:34 -0600

    Descomentar recursos de Azure en archivos de configuración Terraform

--- funtionapp.tf
+++ funtionapp.tf
@@ -1,15 +1,15 @@
-# resource "azurerm_linux_function_app" "serverless1" {
-#   name                = "serverless-${var.project}-${var.environment}"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   location            = var.location
+resource "azurerm_linux_function_app" "serverless1" {
+  name                = "serverless-${var.project}-${var.environment}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
 
-#   storage_account_name       = azurerm_storage_account.saccount2.name
-#   storage_account_access_key = azurerm_storage_account.saccount2.primary_access_key
-#   service_plan_id            = azurerm_service_plan.sp.id
+  storage_account_name       = azurerm_storage_account.saccount2.name
+  storage_account_access_key = azurerm_storage_account.saccount2.primary_access_key
+  service_plan_id            = azurerm_service_plan.sp.id
 
-#   site_config {
-#     application_stack {
-#       python_version = "3.11"
-#     }
-#   }
-# }
+  site_config {
+    application_stack {
+      python_version = "3.11"
+    }
+  }
+}
--- storages.tf
+++ storages.tf
@@ -1,31 +1,31 @@
-# resource "azurerm_storage_account" "saccount" {
-#   name                     = "sa${var.project}${var.environment}"
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   location                 = var.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
+resource "azurerm_storage_account" "saccount" {
+  name                     = "sa${var.project}${var.environment}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
-#   tags = var.tags
-# }
+  tags = var.tags
+}
 
-# resource "azurerm_storage_account" "saccount2" {
-#   name                     = "safuncsettings${var.environment}"
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   location                 = var.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
+resource "azurerm_storage_account" "saccount2" {
+  name                     = "safuncsettings${var.environment}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
-#   tags = var.tags
-# }
+  tags = var.tags
+}
 
 
-# resource "azurerm_storage_container" "c1" {
-#   name                  = "reportes"
-#   container_access_type = "private"
-#   storage_account_id    = azurerm_storage_account.saccount.id
-# }
+resource "azurerm_storage_container" "c1" {
+  name                  = "reportes"
+  container_access_type = "private"
+  storage_account_id    = azurerm_storage_account.saccount.id
+}
 
-# resource "azurerm_storage_queue" "q1" {
-#   name                 = "requests"
-#   storage_account_name = azurerm_storage_account.saccount.name
-# }
+resource "azurerm_storage_queue" "q1" {
+  name                 = "requests"
+  storage_account_name = azurerm_storage_account.saccount.name
+}
